867109ffb5993bb20838ab4ff248feadf6797ff4
stilltrillogf <2004kowalewicz@gmail.com>
2023-02-08 17:42:49 +0100

    Refactor of ChooseLength component

--- src/Components/ChooseLength/ChooseLength.js
+++ src/Components/ChooseLength/ChooseLength.js
@@ -8,52 +8,40 @@ import {
 } from "../Utility/Context";
 import { CHOOSE_LENGTH_ICON_SIZE } from "../Utility/Config";
 
+function ChooseLengthSingle({ type, length }) {
+  const handleChangeLength = useContext(HandleChangeLengthContext);
+
+  return (
+    <div className={styles.ChooseLength}>
+      <h2 className={styles.ChooseLengthName}>{type} length</h2>
+      <div className={styles.ChooseLengthContent}>
+        <span>{length / 60}</span>
+        <div data-id={type} className={`${styles.Icons}`}>
+          <FiChevronUp
+            onClick={handleChangeLength}
+            data-action="Increase"
+            className={styles.Icon}
+            size={CHOOSE_LENGTH_ICON_SIZE}
+          />
+          <FiChevronDown
+            onClick={handleChangeLength}
+            data-action="Decrease"
+            className={styles.Icon}
+            size={CHOOSE_LENGTH_ICON_SIZE}
+          />
+        </div>
+      </div>
+    </div>
+  );
+}
+
 export default function ChooseLength() {
   const breakLength = useContext(BreakLengthContext);
   const sessionLength = useContext(SessionLengthContext);
-  const handleChangeLength = useContext(HandleChangeLengthContext);
   return (
     <div className={styles.Wrapper}>
-      <div className={styles.ChooseLength}>
-        <h2 className={styles.ChooseLengthName}>Break length</h2>
-        <div className={styles.ChooseLengthContent}>
-          <span>{breakLength / 60}</span>
-          <div data-id="Break" className={`${styles.Icons}`}>
-            <FiChevronUp
-              onClick={handleChangeLength}
-              data-action="Increase"
-              className={styles.Icon}
-              size={CHOOSE_LENGTH_ICON_SIZE}
-            />
-            <FiChevronDown
-              onClick={handleChangeLength}
-              data-action="Decrease"
-              className={styles.Icon}
-              size={CHOOSE_LENGTH_ICON_SIZE}
-            />
-          </div>
-        </div>
-      </div>
-      <div className={styles.ChooseLength}>
-        <h2 className={styles.ChooseLengthName}>Session length</h2>
-        <div className={styles.ChooseLengthContent}>
-          <span>{sessionLength / 60}</span>
-          <div data-id="Session" className={styles.Icons}>
-            <FiChevronUp
-              onClick={handleChangeLength}
-              data-action="Increase"
-              className={styles.Icon}
-              size={CHOOSE_LENGTH_ICON_SIZE}
-            />
-            <FiChevronDown
-              onClick={handleChangeLength}
-              data-action="Decrease"
-              className={styles.Icon}
-              size={CHOOSE_LENGTH_ICON_SIZE}
-            />
-          </div>
-        </div>
-      </div>
+      <ChooseLengthSingle type="Break" length={breakLength} />
+      <ChooseLengthSingle type="Session" length={sessionLength} />
     </div>
   );
 }
