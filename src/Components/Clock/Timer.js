aff7424ac4c8a61b39eeb73ca04dde1655e2c532
stilltrillogf <2004kowalewicz@gmail.com>
2023-02-08 08:46:53 +0100

    Timer now becomes red when approaching end of the countdown and alarm sound is played when countdown ends

--- src/Components/Clock/Timer.js
+++ src/Components/Clock/Timer.js
@@ -6,6 +6,10 @@ export default function Timer({ time, setIsRunning, type, setCurrentType }) {
   const [timeLocal, setTimeLocal] = useState(
     type === "Session" ? sessionLength : breakLength
   );
+  const [timerClass, setTimerClass] = useState(styles.Timer);
+  const alarm = new Audio(
+    "https://raw.githubusercontent.com/freeCodeCamp/cdn/master/build/testable-projects-fcc/audio/BeepSound.wav"
+  );
 
   const timeFormatted = new Date(1000 * timeLocal)
     .toISOString()
@@ -14,18 +18,26 @@ export default function Timer({ time, setIsRunning, type, setCurrentType }) {
   useEffect(() => {
     const intervalId = setInterval(() => {
       setTimeLocal((prevTime) => prevTime - 1);
+
+      // When countdown reaches 30 seconds
+      if (timeLocal === 30) {
+        setTimerClass(`${styles.Timer} ${styles.TimerRed}`);
+      }
+
+      // When countdown finishes
       if (timeLocal === 0) {
         // TODO: Here play alarm sound
-        console.log(`${type === "Session" ? "Break time" : "Session time"}`);
+        alarm.play();
         setCurrentType(type === "Session" ? "Break" : "Session");
         setTimeLocal(type === "Session" ? breakLength : sessionLength);
+        setTimerClass(styles.Timer);
       }
     }, 1000);
 
     return () => {
       clearInterval(intervalId);
     };
-  }, [timeLocal, setCurrentType, type, breakLength, sessionLength]);
+  }, [timeLocal, setCurrentType, type, breakLength, sessionLength, alarm]);
 
-  return <div className={styles.Timer}>{timeFormatted}</div>;
+  return <div className={timerClass}>{timeFormatted}</div>;
 }
