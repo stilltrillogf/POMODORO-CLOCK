36b1f4befb1754c08caeb2a9a53cd13ab16067ef
stilltrillogf <2004kowalewicz@gmail.com>
2023-02-08 09:25:38 +0100

    Styling; New font, clock border, hover effects on buttons, adjusted padding/margin of multiple elements

--- src/Components/Clock/Clock.js
+++ src/Components/Clock/Clock.js
@@ -15,20 +15,18 @@ export default function Clock({
     .toISOString()
     .substring(14, 19);
   return (
-    <div className={styles.ClockBorder}>
-      <div className={styles.Clock}>
-        <h2 className={styles.Title}>{type}</h2>
-        {isRunning ? (
-          <Timer
-            type={type}
-            setCurrentType={setCurrentType}
-            time={time}
-            setIsRunning={setIsRunning}
-          />
-        ) : (
-          <div className={styles.Timer}>{timeFormatted}</div>
-        )}
-      </div>
+    <div className={styles.Clock}>
+      <h2 className={styles.Title}>{type}</h2>
+      {isRunning ? (
+        <Timer
+          type={type}
+          setCurrentType={setCurrentType}
+          time={time}
+          setIsRunning={setIsRunning}
+        />
+      ) : (
+        <div className={styles.Timer}>{timeFormatted}</div>
+      )}
     </div>
   );
 }
