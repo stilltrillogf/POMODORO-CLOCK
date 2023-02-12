ffaf9f13fbad1e80752f7b9c45b3340c77b57dee
stilltrillogf <2004kowalewicz@gmail.com>
2023-02-12 10:27:53 +0100

    App instruction; When hovering over button name (play,pause,reset), icon preview appears over it

--- src/Components/InfoModal/ModalContent.js
+++ src/Components/InfoModal/ModalContent.js
@@ -1,12 +1,47 @@
+import { useState } from "react";
+import { FiPlay, FiPause, FiRefreshCw } from "react-icons/fi";
 import styles from "./InfoModal.module.css";
 
 export default function ModalContent() {
+  const [previewIsVisible, setPreviewIsVisible] = useState({
+    play: false,
+    pause: false,
+    refresh: false,
+  });
+
+  function handleIconMouseEnter(e) {
+    if (e.target.dataset.icon === "play") {
+      setPreviewIsVisible({ ...previewIsVisible, play: true });
+    }
+    if (e.target.dataset.icon === "pause") {
+      setPreviewIsVisible({ ...previewIsVisible, pause: true });
+    }
+    if (e.target.dataset.icon === "refresh") {
+      setPreviewIsVisible({ ...previewIsVisible, refresh: true });
+    }
+  }
+  function handleIconMouseOut() {
+    setPreviewIsVisible({ play: false, pause: false, refresh: false });
+  }
+
   return (
     <div data-type="modalContent" className={styles.ModalContent}>
       <h3 data-type="modalContent">How to use the app? It's really simple.</h3>
       <p data-type="modalContent">
         After you customized intervals length to your personal preferences,
-        start the timer by clicking the <b data-type="modalContent">play</b>{" "}
+        start the timer by clicking the{" "}
+        <b
+          className={styles.iconHoverParent}
+          onMouseEnter={handleIconMouseEnter}
+          onMouseOut={handleIconMouseOut}
+          data-type="modalContent"
+          data-icon="play"
+        >
+          {previewIsVisible.play && (
+            <FiPlay className={styles.iconHover} size={25} />
+          )}
+          play
+        </b>{" "}
         icon.
       </p>
       <p data-type="modalContent">
@@ -15,12 +50,35 @@ export default function ModalContent() {
       </p>
       <p>
         You can stop the timer any time by clicking{" "}
-        <b data-type="modalContent">pause</b> icon that appears when the timer
-        is running.
+        <b
+          className={styles.iconHoverParent}
+          onMouseEnter={handleIconMouseEnter}
+          onMouseOut={handleIconMouseOut}
+          data-type="modalContent"
+          data-icon="pause"
+        >
+          {previewIsVisible.pause && (
+            <FiPause className={styles.iconHover} size={25} />
+          )}
+          pause
+        </b>{" "}
+        icon that appears when the timer is running.
       </p>
       <p>In order to change interval length again, you must reset the app.</p>
       <p>
-        To reset the app, click the <b data-type="modalContent">refresh</b>{" "}
+        To reset the app, click the{" "}
+        <b
+          className={styles.iconHoverParent}
+          onMouseEnter={handleIconMouseEnter}
+          onMouseOut={handleIconMouseOut}
+          data-type="modalContent"
+          data-icon="refresh"
+        >
+          {previewIsVisible.refresh && (
+            <FiRefreshCw className={styles.iconHover} size={25} />
+          )}
+          refresh
+        </b>{" "}
         icon.
       </p>
       <h3 data-type="modalContent">Pomodoro technique</h3>
@@ -39,6 +97,7 @@ export default function ModalContent() {
         >
           wikipedia page
         </a>
+        .
       </p>
     </div>
   );
